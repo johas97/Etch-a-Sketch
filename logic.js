cfddc765bfd28fc944d33bf524f60a264c0e7d13
Johan Jig <johan.jigsved@gmail.com>
2021-11-26 11:20:29 +0100

    Added resizable grid

--- logic.js
+++ logic.js
@@ -1,25 +1,40 @@
 const containerSquares = document.querySelector(".containerSquares");
 
-for (i = 1; i < 17; i++){
+function generateRandomColor()
+{
+    var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    console.log(randomColor);
+    return randomColor;
+    //random color will be freshly served
+};
 
-   for (j = 1; j< 17 ; j++){
-   
+function createGrid (gridSize) {
+        containerSquares.style.gridTemplateColumns = `repeat(${gridSize},1fr)`;
+        containerSquares.style.gridTemplateRows = `repeat(${gridSize},1fr)`;
+
+    for (i = 0; i < gridSize * gridSize; i++) {
         const div = document.createElement("div");
         containerSquares.appendChild(div);
         div.addEventListener('mouseover', () => { 
-        div.style.backgroundColor = "black";
-        
+        div.style.backgroundColor = generateRandomColor();
         });
-   } 
-  
+        
+    }
+
 }
+
+createGrid(16);
 // Button interactivity 
 
 const resetButton = document.querySelector(".resetButton");
 resetButton.addEventListener("click", () => {
+    let gridSize = prompt("Enter the new number of boxes in grid.");
+    
+    createGrid(gridSize);
     const divs = containerSquares.querySelectorAll("div");
     divs.forEach( (div) => {
         div.style.backgroundColor = "white";
     });
+   
 });
 
